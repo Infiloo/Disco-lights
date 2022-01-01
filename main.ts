622e9b06028ad8d9a4856263d81189f265fdda80
Infiloo <67906754+Infiloo@users.noreply.github.com>
2022-01-01 15:38:41 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
-basic.showString("Code")
-if (input.logoIsPressed()) {
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . . # . .
-        # . . . #
-        . # # # .
-        `)
-}
+music.playMelody("C D E - E D C - ", 120)
+music.playMelody("C D E F - F E D ", 120)
+music.playMelody("C C C C C C C C ", 120)
 basic.showLeds(`
     . . . . .
     . . . . .
